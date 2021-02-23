0888881cc67460578854cb2cb518d51590d62bc5
Marx <1598240276@qq.com>
2021-02-23 15:55:20 +0800

    no message

--- eat/random0.js
+++ eat/random0.js
@@ -28,7 +28,8 @@
             }, 100);
             run = 1;
         } else {
-           heading.html(heading.html().replace("吃什么？", "吃这个吧！"));
+            heading.html(heading.html().replace("吃什么？", "吃这个吧！"));
+            $("#yaoyiyaoresult").html("恭喜你！摇到了耶O(∩_∩)O ");
             $(this).val("不行，换一个");
             clearInterval(timer);
             run = 0;
